7549296f390867793df2dfff11d06fd888c4f5fe
idleberg <jan@fragnebenan.com>
2018-03-23 16:51:41 +0100

    reindent to 2 spaces

--- mode/nsis.js
+++ mode/nsis.js
@@ -1,22 +1,22 @@
 define(function(require, exports, module) {
-"use strict";
+  "use strict";
 
-var oop = require("../lib/oop");
-var TextMode = require("./text").Mode;
-var NSISHighlightRules = require("./nsis_highlight_rules").NSISHighlightRules;
+  var oop = require("../lib/oop");
+  var TextMode = require("./text").Mode;
+  var NSISHighlightRules = require("./nsis_highlight_rules").NSISHighlightRules;
 // TODO: pick appropriate fold mode
 var FoldMode = require("./folding/cstyle").FoldMode;
 
 var Mode = function() {
-    this.HighlightRules = NSISHighlightRules;
-    this.foldingRules = new FoldMode();
+  this.HighlightRules = NSISHighlightRules;
+  this.foldingRules = new FoldMode();
 };
 oop.inherits(Mode, TextMode);
 
 (function() {
-    this.lineCommentStart = [";", "#"];
-    this.blockComment = {start: "/*", end: "*/"};
-    this.$id = "ace/mode/nsis";
+  this.lineCommentStart = [";", "#"];
+  this.blockComment = {start: "/*", end: "*/"};
+  this.$id = "ace/mode/nsis";
 }).call(Mode.prototype);
 
 exports.Mode = Mode;
