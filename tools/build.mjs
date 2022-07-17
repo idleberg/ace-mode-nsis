c4adaea4855af59e9fa0c056c0e831feb0194544
Jan T. Sott <git@idleberg.com>
2022-07-17 21:44:36 +0200

    fix build, update tooling

---
+++ tools/build.mjs
@@ -0,0 +1,23 @@
+import { join, resolve } from 'path';
+import shell from 'shelljs';
+
+const cacheDir = resolve(process.cwd(), '.cache');
+const distDir = resolve(process.cwd(), 'dist');
+
+function main() {
+    try {
+        shell.cp(resolve(process.cwd(), './src/nsis*.js'), resolve(cacheDir, 'lib/ace/mode/'));
+    } catch (e) {
+        throw Error('Failed to copy nsis mode');
+    }
+
+    shell.exec('./Makefile.dryice.js normal', {
+        cwd: cacheDir
+    });
+
+    shell.mkdir(distDir);
+    shell.cp(resolve(cacheDir, 'build/src/mode-nsis.js'), join(distDir, 'mode-nsis.js'));
+    shell.cp(resolve(cacheDir, 'build/src-noconflict/mode-nsis.js'), join(distDir, 'mode-nsis.noconflict.js'));
+}
+
+main();
